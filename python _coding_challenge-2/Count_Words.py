c8af1987e008f08b93e16c9397526321028840bc
Kanika Chaudhary <kanikac132@gmail.com>
2026-04-09 10:27:11 +0530

    program to count words in sentence

---
+++ python _coding_challenge-2/Count_Words.py
@@ -0,0 +1,11 @@
+# program to count words in sentence
+# Taking input from user
+s = input("Enter sentence: ")
+
+words = s.split()
+count = 0
+
+for i in words:
+    count += 1
+
+print("Words =", count)
