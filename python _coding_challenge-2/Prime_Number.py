3361ca8020b45ff511470b0d6f481e8d23c1b3ea
Kanika Chaudhary <kanikac132@gmail.com>
2026-04-09 11:20:23 +0530

    function to check prime number

---
+++ python _coding_challenge-2/Prime_Number.py
@@ -0,0 +1,17 @@
+#  function to check prime number
+# Taking input from user
+num = int(input("Enter number: "))
+
+if num < 2:
+    print("Not Prime")
+else:
+    is_prime = True
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print("Prime")
+    else:
+        print("Not Prime")
