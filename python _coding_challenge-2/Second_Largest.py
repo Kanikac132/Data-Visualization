20b1cd9241498aec6f841248770faf75420d1104
Kanika Chaudhary <kanikac132@gmail.com>
2026-04-09 10:34:36 +0530

    program to find second largest element

---
+++ python _coding_challenge-2/Second_Largest.py
@@ -0,0 +1,14 @@
+#  program to find second largest element
+# Taking input from user
+lst = list(map(int, input("Enter numbers: ").split()))
+
+largest = second = -99999
+
+for i in lst:
+    if i > largest:
+        second = largest
+        largest = i
+    elif i > second and i != largest:
+        second = i
+
+print("Second Largest =", second)
