ca125275f4700f167d43086b362aeddcb11d8e60
Kanika Chaudhary <kanikac132@gmail.com>
2026-04-09 09:48:46 +0530

    program to check if a character is a vowel

---
+++ python _coding_challenge-2/Vowel_Check.py
@@ -0,0 +1,8 @@
+#  program to check if a character is a vowel
+# Taking input from user
+ch = input("Enter character: ")
+
+if ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u':
+    print("Vowel")
+else:
+    print("Consonant")
