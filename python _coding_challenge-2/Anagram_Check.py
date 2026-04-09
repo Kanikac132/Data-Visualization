5f15febdd728d3fcb2587aabd566639ac4b31a4e
Kanika Chaudhary <kanikac132@gmail.com>
2026-04-09 10:25:22 +0530

    program to verify two strings are anagrams

---
+++ python _coding_challenge-2/Anagram_Check.py
@@ -0,0 +1,9 @@
+#  program to verify two strings are anagrams
+# Taking input from user
+a = input("Enter first string: ")
+b = input("Enter second string: ")
+
+if sorted(a) == sorted(b):
+    print("True")
+else:
+    print("False")
