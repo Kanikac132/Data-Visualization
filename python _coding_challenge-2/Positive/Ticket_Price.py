2f73a560ac6ef2667b88535dedfcbe66eba30edf
Kanika Chaudhary <kanikac132@gmail.com>
2026-04-09 09:52:54 +0530

    program to determine ticket price

---
+++ python _coding_challenge-2/Positive/Ticket_Price.py
@@ -0,0 +1,10 @@
+#  program to determine ticket price
+# Taking input from user
+day = input("Enter day: ")
+
+if day == "Sunday" or day == "sunday":
+    price = 200
+else:
+    price = 100
+
+print("Ticket Price =", price)
