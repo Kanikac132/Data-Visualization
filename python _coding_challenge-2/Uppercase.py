606d408e67bdfd8c190ecb47d2b9d1d2eabf8ea6
Kanika Chaudhary <kanikac132@gmail.com>
2026-04-09 10:20:23 +0530

    program to convert string to uppercase without built-in methods

---
+++ python _coding_challenge-2/Uppercase.py
@@ -0,0 +1,12 @@
+# program to convert string to uppercase without built-in methods
+# Taking input from user
+s = input("Enter string: ")
+
+res = ""
+for ch in s:
+    if 'a' <= ch <= 'z':
+        res += chr(ord(ch) - 32)
+    else:
+        res += ch
+
+print(res)
