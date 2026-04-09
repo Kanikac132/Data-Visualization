17d70d8ca33c1f12ce9999742def328800fed595
Kanika Chaudhary <kanikac132@gmail.com>
2026-04-09 10:22:23 +0530

    program to find longest word

---
+++ python _coding_challenge-2/Longest_Word.py
@@ -0,0 +1,10 @@
+#  program to find longest word
+# Taking input from user
+s = input("Enter sentence: ").split()
+
+longest = s[0]
+for word in s:
+    if len(word) > len(longest):
+        longest = word
+
+print("Longest word =", longest)
