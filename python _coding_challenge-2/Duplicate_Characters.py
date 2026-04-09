fd46b66ba618303fe934f4833e58311c690208d6
Kanika Chaudhary <kanikac132@gmail.com>
2026-04-09 10:28:39 +0530

    program to find duplicate characters

---
+++ python _coding_challenge-2/Duplicate_Characters.py
@@ -0,0 +1,9 @@
+#  program to find duplicate characters
+# Taking input from user
+s = input("Enter string: ")
+
+printed = ""
+for ch in s:
+    if s.count(ch) > 1 and ch not in printed:
+        print(ch, end=" ")
+        printed += ch
