7b3ea01dddbbcccafec6340f599c4dfa97059e4b
Kanika Chaudhary <kanikac132@gmail.com>
2026-04-09 10:36:04 +0530

    program to count unique elements

---
+++ python _coding_challenge-2/Count_Unique.py
@@ -0,0 +1,10 @@
+#  program to count unique elements
+# Taking input from user
+lst = list(map(int, input("Enter numbers: ").split()))
+
+unique = []
+for i in lst:
+    if i not in unique:
+        unique.append(i)
+
+print("Unique count =", len(unique))
