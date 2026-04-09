6187dee12aa976331817060e24666a00142c340f
Kanika Chaudhary <kanikac132@gmail.com>
2026-04-09 10:31:58 +0530

    program to remove duplicates from list

---
+++ python _coding_challenge-2/Remove_Duplicates_from_List.py
@@ -0,0 +1,10 @@
+# program to remove duplicates from list
+# Taking input from user
+lst = list(map(int, input("Enter list: ").split()))
+
+res = []
+for i in lst:
+    if i not in res:
+        res.append(i)
+
+print(res)
