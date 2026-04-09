62ccff052a00a5a48a3593799c0754c9a2523aca
Kanika Chaudhary <kanikac132@gmail.com>
2026-04-09 10:38:22 +0530

    program to sort list without using sort()

---
+++ python _coding_challenge-2/Sort_without_sort().py
@@ -0,0 +1,10 @@
+# program to sort list without using sort()
+# Taking input from user
+lst = list(map(int, input("Enter numbers: ").split()))
+
+for i in range(len(lst)):
+    for j in range(i+1, len(lst)):
+        if lst[i] > lst[j]:
+            lst[i], lst[j] = lst[j], lst[i]
+
+print(lst)
